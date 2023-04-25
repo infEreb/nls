744bc8095797eac98e2d5b6d995c41f34c58c12c
infEreb <savushkin.egor.00027@gmail.com>
2023-04-25 22:08:57 +0300

    ci/cd adv added

--- deploy/dev/deploy.sh
+++ deploy/dev/deploy.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 BUILD_VERSION=1.0.0
 
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
+
 docker build -t nls-build ./nls-build
-docker tag nls-build nls/nls-build:$BUILD_VERSION
-#docker push nls/nls-build:$BUILD_VERSION
+docker tag nls-build localhost:5000/nls-build:$BUILD_VERSION
+docker push localhost:5000/nls-build:$BUILD_VERSION
 
 docker run -d \
-    -v build:/build
+    -v build:/build \
     nls-build:$BUILD_VERSION
 
 # Netflow
 docker build -t nls-netflow ./build/netflow
-docker tag nls-netflow nls/nls-netflow:$BUILD_VERSION
+docker tag nls-netflow localhost:5000/nls-netflow:$BUILD_VERSION
+docker push localhost:5000/nls-netflow:$BUILD_VERSION
 # Netflow Agent
 docker build -t nls-netflow-agent ./build/netflow/agent
-docker tag nls-netflow-agent nls/nls-netflow-agent:$BUILD_VERSION
+docker tag nls-netflow-agent localhost:5000/nls-netflow-agent:$BUILD_VERSION
+docker push localhost:5000/nls-netflow-agent:$BUILD_VERSION
 # Netflow Ethernet
 docker build -t nls-netflow-ethernet ./build/netflow/ethernet
-docker tag nls-netflow-ethernet nls/nls-netflow-ethernet:$BUILD_VERSION
+docker tag nls-netflow-ethernet localhost:5000/nls-netflow-ethernet:$BUILD_VERSION
+docker push localhost:5000/nls-netflow-ethernet:$BUILD_VERSION
