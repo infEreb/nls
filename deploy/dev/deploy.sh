7f29e7ebcd1906b8bed5f19f497b8cb52e6beec9
infEreb <savushkin.egor.00027@gmail.com>
2023-04-25 22:31:33 +0300

    ci/cd adv added

--- deploy/dev/deploy.sh
+++ deploy/dev/deploy.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
 BUILD_VERSION=1.0.0
 
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
-docker build -t nls-build ./nls-build
-docker tag nls-build localhost:5000/nls-build:$BUILD_VERSION
+docker build -t nls-build:$BUILD_VERSION ./nls-build
+docker tag nls-build:$BUILD_VERSION localhost:5000/nls-build:$BUILD_VERSION
 docker push localhost:5000/nls-build:$BUILD_VERSION
 
 docker run -d \
-    -v build:/build \
+    -v $(pwd)/build:/build \
+    --name build \
     nls-build:$BUILD_VERSION
 
 # Netflow
-docker build -t nls-netflow ./build/netflow
-docker tag nls-netflow localhost:5000/nls-netflow:$BUILD_VERSION
+docker build -t nls-netflow:$BUILD_VERSION ./build/netflow
+docker tag nls-netflow:$BUILD_VERSION localhost:5000/nls-netflow:$BUILD_VERSION
 docker push localhost:5000/nls-netflow:$BUILD_VERSION
 # Netflow Agent
-docker build -t nls-netflow-agent ./build/netflow/agent
-docker tag nls-netflow-agent localhost:5000/nls-netflow-agent:$BUILD_VERSION
+docker build -t nls-netflow-agent:$BUILD_VERSION ./build/netflow/agent
+docker tag nls-netflow-agent:$BUILD_VERSION localhost:5000/nls-netflow-agent:$BUILD_VERSION
 docker push localhost:5000/nls-netflow-agent:$BUILD_VERSION
 # Netflow Ethernet
-docker build -t nls-netflow-ethernet ./build/netflow/ethernet
-docker tag nls-netflow-ethernet localhost:5000/nls-netflow-ethernet:$BUILD_VERSION
+docker build -t nls-netflow-ethernet:$BUILD_VERSION ./build/netflow/ethernet
+docker tag nls-netflow-ethernet:$BUILD_VERSION localhost:5000/nls-netflow-ethernet:$BUILD_VERSION
 docker push localhost:5000/nls-netflow-ethernet:$BUILD_VERSION
--- deploy/dev/nls-build/build.sh
+++ deploy/dev/nls-build/build.sh
@@ -14,32 +14,41 @@ fi
 
 git clone https://github.com/infEreb/nls.git $REPO_DIR
 # Get NETFLOW repo
-NETFLOW_REPO_DIR=$REPO_DIR/netflow/server/app
+NETFLOW_REPO_DIR=$REPO_DIR/netflow/server
 NETFLOW_DIR=$BUILD_DIR/netflow
+echo "Variable NETFLOW_REPO_DIR is $NETFLOW_REPO_DIR"
 if [ ! -d $NETFLOW_DIR ]; then
+    echo "Not found $NETFLOW_DIR directory, creating this one..."
     mkdir -p $NETFLOW_DIR/configs
 fi
-cp $NETFLOW_REPO_DIR/netflow/Dockerfile $NETFLOW_DIR
+echo "Copying netflow data..."
+cp $NETFLOW_REPO_DIR/Dockerfile $NETFLOW_DIR
+
+NETFLOW_REPO_DIR=$NETFLOW_REPO_DIR/app
+echo "Changing NETFLOW_REPO_DIR to $NETFLOW_REPO_DIR"
+cd $NETFLOW_REPO_DIR
 
 # Agent build
 AGENT_DIR=$NETFLOW_DIR/agent
 if [ ! -d $AGENT_DIR ]; then
+    echo "Not found $AGENT_DIR directory, creating this one..."
     mkdir -p $AGENT_DIR/configs
 fi
-
+echo "Copying netflow/agent data..."
 cp $NETFLOW_REPO_DIR/agent/configs/* $AGENT_DIR/configs
 cp $NETFLOW_REPO_DIR/agent/Dockerfile $AGENT_DIR
-go env GOBIN=$AGENT_DIR
-go install $AGENT_DIR/cmd/agent
+go env -w GOBIN=$AGENT_DIR
+go install ./agent/cmd/agent
 
 
 # Ethernet build
 ETHERNET_DIR=$NETFLOW_DIR/ethernet
 if [ ! -d $ETHERNET_DIR ]; then
+    echo "Not found $ETHERNET_DIR directory, creating this one..."
     mkdir -p $ETHERNET_DIR/configs
 fi
-
+echo "Copying netflow/ethernet data..."
 cp $NETFLOW_REPO_DIR/ethernet/configs/* $ETHERNET_DIR/configs
 cp $NETFLOW_REPO_DIR/ethernet/Dockerfile $ETHERNET_DIR
-go env GOBIN=$ETHERNET_DIR
-go install $ETHERNET_DIR/cmd/ethernet
+go env -w GOBIN=$ETHERNET_DIR
+go install ./ethernet/cmd/ethernet
